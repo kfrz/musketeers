b1fdd260c6fbdb1d1cb0d59a5fa1073407d35e41
Keifer Furzland <kfrz.code@gmail.com>
2018-06-13 06:41:13 -0500

    Add a few more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
-['Roland Deschain', 'Mordred Deschain', 'Elon Musk', 'Jeff Bezos'].each do |name|
+['Roland Deschain',
+ 'Mordred Deschain',
+ 'Elon Musk',
+ 'Jeff Bezos',
+ 'Mark Zuckerberg',
+ 'John Denver',
+ 'Tom Hanks',
+ 'Clark Kent',
+ 'Arthur Fonzarelli',
+ 'Jack Bauer'].each do |name|
+
   first, last = name.downcase.split(' ')
   tld = last.slice!(-2..-1)
   email = "#{first}@#{last}.#{tld}"
-  Gunslinger.create(first_name: name.split[0], last_name: name.split[1], email: email)
+
+  next if Gunslinger.find_by(email: email)
+  Gunslinger.create(first_name: name.split[0],
+                    last_name: name.split[1],
+                    email: email)
 end
